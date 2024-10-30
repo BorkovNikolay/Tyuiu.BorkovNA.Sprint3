c45c77baa02f6c45c32a5a42c191eff53897d457
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-10-31 02:30:22 +0500

    task4

---
+++ Tyuiu.BorkovNA.Sprint3.Task4.V28.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.BorkovNA.Sprint3.Task4.V28.Lib
+{
+    public class DataService : ISprint3Task4V28
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            int x;
+            double sp = 0;
+            int i = 0;
+            for (x = startValue; x <= stopValue; x++)
+            {
+
+              if (x == 0)
+                { 
+                continue;
+                }
+
+                sp += (Math.Cos(x)+Math.Sin(x))/x;
+                i++;
+            }
+            return Math.Round(sp, 3);
+
+        }
+    }
+}
---
+++ Tyuiu.BorkovNA.Sprint3.Task4.V28/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Tyuiu.BorkovNA.Sprint3.Task4.V28.Lib;
+
+
+
+
+namespace Tyuiu.BorkovNA.Sprint3.Task4.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Cпринт #2 | Выполнил: Борков Н. А. | пктб-24-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                *");
+            Console.WriteLine("* Тема:                                                                    *");
+            Console.WriteLine("* Задание #4                                                               *");
+            Console.WriteLine("* Вариант #28                                                              *");
+            Console.WriteLine("* Выполнил: Борков Н. А. | пктб-24-1                                       *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Введите startValue =                                                     *");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("* Введите stopValue =                                                      *");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            double res = ds.Calculate(x, y);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+
+}
