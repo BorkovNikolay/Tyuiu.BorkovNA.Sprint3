52be4075c1bd09ffec26386cfdc38e7e4b095499
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-10-31 05:36:00 +0500

    task7

--- Tyuiu.BorkovNA.Sprint3.Task7.V11.Test/DataServiceTest.cs
+++ Tyuiu.BorkovNA.Sprint3.Task7.V11.Test/DataServiceTest.cs
@@ -1,3 +1,5 @@
+using Tyuiu.BorkovNA.Sprint3.Task7.V11.Lib;
+
 namespace Tyuiu.BorkovNA.Sprint3.Task7.V11.Test
 {
     [TestClass]
@@ -6,7 +8,10 @@ namespace Tyuiu.BorkovNA.Sprint3.Task7.V11.Test
         [TestMethod]
         public void TestMethod1()
         {
-
+            double[] res = { 20.36, 15.42, 10.99, 7.43, 4.33, 1, 0, -8.87, -13.03, -16.53, -19.6};
+            DataService ds = new();
+            Assert.AreEqual(res, ds.GetMassFunction(-5, 5));
+          
         }
     }
 }
